f4bff1de2b001b3f44015f2b915aa1da797c3842
Doge <128231475+Dogeisveryop@users.noreply.github.com>
2023-08-06 10:47:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function Glanza() {
+   TOYOTa = document.getElementById("TOYOTA").innerHTML = "<h1 style='color: red; background-color:azure ; position: fixed; border-radius: 10px ; font-size: 30px; cursor: pointer;' id='TOYOTA'>Glanza</h1>"
+}
+
+function Urban() {
+    TOYOTa = document.getElementById("TOYOTA").innerHTML = "<h1 style='color: red; background-color:azure ; position: fixed; border-radius: 10px ; font-size: 30px; cursor: pointer;' id='TOYOTA'>Urban cruiser hyryder</h1>"
+}
+
+function Innova() {
+    TOYOTa = document.getElementById("TOYOTA").innerHTML = "<h1 style='color: red; background-color:azure ; position: fixed; border-radius: 10px ; font-size: 30px; cursor: pointer;' id='TOYOTA'>Innova hycross</h1>"
+}
+
+function Hilux() {
+    TOYOTa = document.getElementById("TOYOTA").innerHTML = "<h1 style='color: red; background-color:azure ; position: fixed; border-radius: 10px ; font-size: 30px; cursor: pointer;' id='TOYOTA'>Hilux</h1>"
+}
+
+function Fortuner() {
+    TOYOTa = document.getElementById("TOYOTA").innerHTML = "<h1 style='color: red; background-color:azure ; position: fixed; border-radius: 10px ; font-size: 30px; cursor: pointer;' id='TOYOTA'>Fortuner</h1>"
+}
+
+function Legender() {
+    TOYOTa = document.getElementById("TOYOTA").innerHTML = "<h1 style='color: red; background-color:azure ; position: fixed; border-radius: 10px ; font-size: 30px; cursor: pointer;' id='TOYOTA'>Legender</h1>"
+}
+
+function Camry() {
+    TOYOTa = document.getElementById("TOYOTA").innerHTML = "<h1 style='color: red; background-color:azure ; position: fixed; border-radius: 10px ; font-size: 30px; cursor: pointer;' id='TOYOTA'>Camry</h1>"
+}
+
+function Vellfire() {
+    TOYOTa = document.getElementById("TOYOTA").innerHTML = "<h1 style='color: red; background-color:azure ; position: fixed; border-radius: 10px ; font-size: 30px; cursor: pointer;' id='TOYOTA'>Vellfire</h1>"
+}
+
+
+
+
+
